fe5b64479d24f75bb09a54dfe8b39234e2d568e0
JohnnyDu0815 <39909450+JohnnyDu0815@users.noreply.github.com>
2026-02-15 20:51:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,4 +23,4 @@
 
 #git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 
-#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
