4356df8e2bc2f0e9e54687c92fa1f020be866971
JohnnyDu0815 <39909450+JohnnyDu0815@users.noreply.github.com>
2026-02-15 23:31:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,4 +25,4 @@
 
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
-sed -i '1i src-git openclash https://github.com/vernesong/openclash' feeds.conf.default
+#sed -i '1i src-git openclash https://github.com/vernesong/openclash' feeds.conf.default
