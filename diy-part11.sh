40b0b3600e542bfa8a786597f7d4b31179cb6ff5
JohnnyDu0815 <39909450+JohnnyDu0815@users.noreply.github.com>
2026-02-26 10:31:15 +0800

    Create diy-part11.sh

---
+++ diy-part11.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# Add ADGuardHome source
+#git clone https://github.com/JohnnyDu0815/luci-app-adguardhome package/luci-app-adguardhome
+#chmod -R 755 ./package/luci-app-adguardhome/*
+
+#git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+
+#sed -i '1i src-git openclash https://github.com/vernesong/openclash' feeds.conf.default
+
+git_sparse_clone luci https://github.com/chenmozhijin/turboacc luci-app-turboacc
